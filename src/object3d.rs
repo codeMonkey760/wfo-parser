d04e82d65c02b103560bcb2fa00b01f3838bea5e
Sean Paget <codeMonkey760@gmail.com>
2024-10-19 12:28:28 -0700

    Add struct for 3d objects

---
+++ src/object3d.rs
@@ -0,0 +1,19 @@
+use crate::vertex_format::{VertexFormat};
+
+pub(crate) struct Object3d {
+    pub name: String,
+    pub format: VertexFormat,
+    pub vertex_buffer: Vec<f64>,
+    pub index_buffer: Vec<u64>,
+}
+
+impl Object3d {
+    pub(crate) fn from(name: String) -> Self {
+        Self {
+            name,
+            format: VertexFormat::Unknown,
+            vertex_buffer: Vec::new(),
+            index_buffer: Vec::new(),
+        }
+    }
+}
