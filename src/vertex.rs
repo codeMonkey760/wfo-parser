cc702563eede7c8952bda97e894cff67aa8c28de
Sean Paget <codeMonkey760@gmail.com>
2024-11-04 10:42:22 -0800

    Fix errant format detection when parsing face statements

--- src/vertex.rs
+++ src/vertex.rs
@@ -12,11 +12,11 @@ pub(crate) enum VertexFormat {
 impl VertexFormat {
     pub(crate) fn from_indices(indices: &(u64, u64, u64)) -> Self {
         match indices {
-            (0, n, tc) => { panic!("Vertex format must have position index"); },
+            (0, tc, n) => { panic!("Vertex format must have position index"); },
             (p, 0, 0) => VertexFormat::VertexP,
-            (p, n, 0) => VertexFormat::VertexPN,
-            (p, 0, tc) => VertexFormat::VertexPT,
-            (ps, n, tc) => VertexFormat::VertexPNT,
+            (p, tc, 0) => VertexFormat::VertexPT,
+            (p, 0, n) => VertexFormat::VertexPN,
+            (ps, tc, n) => VertexFormat::VertexPNT,
         }
     }
 }
